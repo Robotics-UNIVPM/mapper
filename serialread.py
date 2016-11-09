755489c9d353bf20bce89cd11d939896fe59b0db
Loris Rossi <lorisrossirn@gmail.com>
2016-11-09 21:35:42 +0100

    modificato il delay e aggiunti commenti

--- serialread.py
+++ serialread.py
@@ -1,7 +1,9 @@
 # UTILIZZO:
 # 1. Modificare le variabili "port" e "baud" in base alla vostra macchina e Arduino
-# 2. Lanciare il programma da terminale con "python serialread.py"
-# 3. Seguire le indicazioni
+# 2. Se si vogliono ricevere più dati simultaneamente, su Arduino è necessario
+#    scrivere su una riga tutti i dati, separati da ';'
+# 3. Lanciare il programma da terminale con "python serialread.py"
+# 4. Seguire le indicazioni
 #
 # Il nome del file di salvataggio va scritto SENZA estensione (viene salvato in .csv)
 
@@ -10,11 +12,6 @@ import serial, csv, time
 port = '/dev/ttyACM0'
 baud = 115200
 
-def delay(tempo):
-    t0 = time.time()
-    while (time.time() - t0) < tempo:
-        x = 0 # modo brutto per far passare il tempo
-
 
 tempo = input('Inserisci intervallo di tempo (secondi): ')
 
@@ -22,7 +19,7 @@ registro = [] # matrice per salvare i dati
 
 ## Inizializzazione porta seriale
 ser = serial.Serial(port, baudrate=baud)
-delay(1) # serve per far resettare bene il buffer
+time.sleep(1) # serve per far resettare bene il buffer
 ser.reset_input_buffer()
 y = ser.readline() # scarta il primo valore dal buffer
 
@@ -30,7 +27,7 @@ print 'Sto ricevendo i dati..'
 t0 = time.time()
 while (time.time() - t0) < tempo:
     x = ser.readline()[:-2] # [:-2] pulisce i caratteri di newline
-    y = x.split(';')
+    y = x.split(';') # separa i valori ricevuti
     registro.append(y)
 
 print "Fatto!"
