39b2cb76660db1d7c8d7045db27099c4d8917825
sebgiles <mrsebgiles@gmail.com>
2016-11-15 17:00:05 +0100

    realtime matlab
    abbozzo per grafici in tempo reale con matlab

---
+++ tools/realtime.m
@@ -0,0 +1,14 @@
+delete(instrfindall)
+h=animatedline;
+h.MaximumNumPoints = 1000;
+arduino = serial('/dev/cu.usbmodem1411');
+arduino.BaudRate = 115200;
+fopen(arduino);
+for i=1:1000
+    data = str2num(fscanf(arduino));
+    addpoints(h,data(1,1)/1000,data(1,2));
+    drawnow
+end
+fclose(arduino)
+delete(arduino)
+clear arduino
