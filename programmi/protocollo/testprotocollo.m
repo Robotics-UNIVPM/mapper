ab6aa28060a443ffa319617112ee52285d0f9777
sebgiles <mrsebgiles@gmail.com>
2016-12-12 19:29:10 +0100

    test controllo errori

---
+++ programmi/protocollo/testprotocollo.m
@@ -0,0 +1,40 @@
+%Per testare collegarsi in telnet localmente
+% 'telnet 127.0.0.1 2300'
+
+t = tcpip('127.0.0.1', 2300, 'NetworkRole', 'server');
+fopen(t);
+
+ID_1 = 65;      %'A'
+ID_2 = 66;      %'B'
+ID_END = 90;    %'Z'
+
+
+
+x2 = 0;
+x1 = 0;
+data = [];
+for i = 1:100
+    while x2 ~= ID_2
+        if x2 == ID_1
+            x1 = x2;
+        end
+        while x1 ~= ID_1
+            x1 = fread(t, 1, 'uint8');
+        end
+        x1 = 0;
+        x2 = fread(t, 1, 'uint8');
+    end
+    x2 = 0;
+
+    var = fread(t,1,'uint8');
+
+    check2 = fread(t, 1, 'uint8');
+    if check2 ~= ID_END
+        x1 = check2;
+        continue
+    end
+
+    disp(var);
+
+
+end
